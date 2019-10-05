80822a791d6684b406d0ddb6bb16107eb8bec7a1
Emma-yuan <liuyuan_2018@foxmail.com>
2019-10-05 19:10:20 +0800

    xxx

---
+++ build/precache-manifest.9aacc14ecc555396e0d9e061f7cd87bf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56772f0b5f35e4de3be79649ba25f840",
+    "url": "/index.html"
+  },
+  {
+    "revision": "661be23058570ae2a385",
+    "url": "/static/css/main.dcf524ca.chunk.css"
+  },
+  {
+    "revision": "adc048b0ca205798a492",
+    "url": "/static/js/2.7d0e28a3.chunk.js"
+  },
+  {
+    "revision": "661be23058570ae2a385",
+    "url": "/static/js/main.ae70f796.chunk.js"
+  },
+  {
+    "revision": "ff7e631346c12c639492",
+    "url": "/static/js/runtime-main.7677ad42.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
 
 function App() {
   return (
-    <div className="App">
-      Todo
+    <div>
+      <CssBaseline />
+      <div className="App">
+        Todo
+      </div>
+      <Typography variant="h1" component="h2">
+      h1. Heading
+      </Typography>
+      <Button variant="contained" color="primary">
+        hello,world
+      </Button>
     </div>
+
   );
 }
 
